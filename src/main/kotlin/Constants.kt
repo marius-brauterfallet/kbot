ffbbe875255b442f6f0e5577197eac9e433720b5
Marius Brauterfallet <marius.brauterfallet@tietoevry.com>
2024-06-21 22:00:58 +0200

    implement simple role addition based on reactions

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,7 @@
+object Constants {
+    val rolesMessageId = 1253766892091605146L
+    val roleEmojis = mapOf(
+        "🧠" to "GENIUS",
+        "🤠" to "Idiot"
+    )
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import discord4j.core.DiscordClient
 import discord4j.core.event.domain.message.MessageCreateEvent
+import discord4j.core.event.domain.message.ReactionAddEvent
+import discord4j.core.event.domain.message.ReactionRemoveEvent
 
 fun main() {
     val token = System.getenv("kimmoToken")
@@ -17,6 +19,38 @@ fun main() {
 
         if (message.content == "!ping") {
             message.channel.block()?.createMessage("Pong!")?.block()
+        }
+    }
+
+    gateway.on(ReactionAddEvent::class.java).subscribe { event ->
+        val message = event.messageId
+        val emoji = event.emoji
+
+        if (message.asLong() == Constants.rolesMessageId) {
+            val guild = event.guild.block() ?: return@subscribe
+            val member = event.user.block()?.asMember(guild.id)?.block() ?: return@subscribe
+            val roles = guild.roles.collectList().block() ?: return@subscribe
+            val roleName = Constants.roleEmojis[emoji.asUnicodeEmoji().get().raw] ?: return@subscribe
+
+            runCatching {
+                member.addRole(roles.single { it.name == roleName }.id).block()
+            }
+        }
+    }
+
+    gateway.on(ReactionRemoveEvent::class.java).subscribe { event ->
+        val message = event.messageId
+        val emoji = event.emoji
+
+        if (message.asLong() == Constants.rolesMessageId) {
+            val guild = event.guild.block() ?: return@subscribe
+            val member = event.user.block()?.asMember(guild.id)?.block() ?: return@subscribe
+            val roles = guild.roles.collectList().block() ?: return@subscribe
+            val roleName = Constants.roleEmojis[emoji.asUnicodeEmoji().get().raw] ?: return@subscribe
+
+            runCatching {
+                member.removeRole(roles.single { it.name == roleName }.id).block()
+            }
         }
     }
 
