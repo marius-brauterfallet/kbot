f8e6c3231417fa32a442a2bac85e8a3132c80535
Marius Brauterfallet <marius.brauterfallet@tietoevry.com>
2024-06-21 17:27:01 +0200

    implement ping command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
+import discord4j.core.DiscordClient
+import discord4j.core.event.domain.message.MessageCreateEvent
+
 fun main() {
-    println("Hello, World!")
+    val token = System.getenv("kimmoToken")
+
+    val client = DiscordClient.create(token)
+    val gateway = client.login().block()
+
+    if (gateway == null) {
+        println("Something went wrong??")
+        return
+    }
+
+    gateway.on(MessageCreateEvent::class.java).subscribe {
+        val message = it.message
+
+        if (message.content == "!ping") {
+            message.channel.block()?.createMessage("Pong!")?.block()
+        }
+    }
+
+    gateway.onDisconnect().block()
 }
