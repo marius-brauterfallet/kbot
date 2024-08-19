2d0eeeb8164ce2633cf56b6231ee45921f7da161
Marius Brauterfallet <m.brauterfallet@gmail.com>
2024-08-19 19:54:32 +0200

    implement dependency injection of properties

---
+++ src/main/kotlin/KbotProperties.kt
@@ -0,0 +1,6 @@
+import java.util.*
+
+class KbotProperties(properties: Properties) {
+    val appVersion = properties.getProperty("version")
+        ?: throw IllegalStateException("Something went wrong when retrieving app properties")
+}
